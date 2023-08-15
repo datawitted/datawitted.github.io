627062fdf6e222a4452a586898291ae9c12ae809
Olasunkanmi Aremu <119086951+datawitted@users.noreply.github.com>
2023-08-15 04:41:05 +0100

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,9 @@
+// Function to open the project popup
+function openPopup() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+// Function to close the project popup
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
