a5e4b68be106dc8f4655f0eaa54643d4a1c9d30d
Olasunkanmi Aremu <119086951+datawitted@users.noreply.github.com>
2023-08-16 03:50:05 +0100

    Delete popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-// JavaScript for displaying and hiding the popup
-function openPopup(popupId) {
-    document.getElementById(popupId).style.display = "block";
-}
-
-function closePopup(popupId) {
-    document.getElementById(popupId).style.display = "none";
-}
